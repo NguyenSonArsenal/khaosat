b35418c9bbdeae7c191bffbd476b810d0b1d3999
sonnv <vanson297.nguyen@gmail.com>
2024-12-23 22:47:50 +0700

    'add'

--- database/migrations/2024_19_12_005_create_survey_options_table.php
+++ database/migrations/2024_19_12_005_create_survey_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChoicesTable extends Migration
+class CreateSurveyOptionsTable extends Migration
 {
     /**
      * Run the migrations.
